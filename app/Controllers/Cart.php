319d4c0a6b4f80468d813f8a38420e8b3d81a858
vin0414 <vinmogate@gmail.com>
2024-05-21 08:40:36 +0800

    added model and controller

---
+++ app/Controllers/Cart.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controllers;
+
+class Cart extends BaseController
+{
+    private $db;
+    public function __construct()
+    {
+        $this->db = db_connect();
+    }
+
+    public function viewCart()
+    {
+        $builder = $this->db->table('tblblog a');
+        $builder->select('a.*,b.Fullname');
+        $builder->join('tblaccount b','b.accountID=a.accountID','LEFT');
+        $builder->orderBy('a.blogID','DESC')->limit(3);
+        $blog = $builder->get()->getResult();
+
+        $data['items'] = is_array(session('cart'))?array_values(session('cart')):array();
+        $data['total'] = $this->total();
+        $data['blog'] = $blog;
+        return view('welcome_message',$data);
+    }
+
+    public function details($id)
+    {
+        $builder = $this->db->table('tblproduct a');
+        $builder->select('a.*,b.CategoryName');
+        $builder->join('tblcategory b','b.categoryID=a.categoryID','LEFT');
+        $builder->WHERE('a.productID',$id);
+        $products = $builder->get()->getResult();
+        $data = ['product'=>$products];
+        return view('cart/details',$data);
+    }
+
+    public function remove($id)
+    {
+        $index = $this->exists($id);
+        $cart = array_values(session('cart'));
+        unset($cart[$index]);
+        $session = session();
+        $session->set('cart',$cart);
+        return $this->response->redirect(site_url('welcome_message'));
+    }
+
+    public function buy($id)
+    {
+        $productModel = new \App\Models\productModel();
+        $product = $productModel->find($id);
+        $item = array(
+            'id'=>$product['productID'],
+            'name'=>$product['productName'],
+            'photo'=>$product['Image'],
+            'price'=>$product['UnitPrice'],
+            'quantity'=>$this->request->getGet('qty')
+        );
+        $session = session();
+        if($session->has('cart'))
+        {
+            $index = $this->exists($id);
+            $cart = array_values(session('cart'));
+            if($index == -1)
+            {
+                array_push($cart, $item);
+            }
+            else
+            {
+                session()->setFlashdata('fail','Invalid! Item(s) already added in your cart');
+                return redirect()->to('/products')->withInput();
+            }
+            $session->set('cart',$cart);
+        }
+        else
+        {
+            $cart = array($item);
+            $session->set('cart',$cart);
+        }
+        return $this->response->redirect(site_url('welcome_message'));
+    }
+
+    private function exists($id)
+    {
+        $items = array_values(session('cart'));
+        for($i = 0; $i < count($items); $i++)
+        {
+            if($items[$i]['id']==$id)
+            {
+                return $i;
+            }
+        }
+        return -1;
+    }
+
+    private function total()
+    {
+        $s = 0;
+        $items = is_array(session('cart'))?array_values(session('cart')):array();
+        foreach($items as $item)
+        {
+            $s += $item['price']*$item['quantity'];
+        }
+        return $s;
+    }
+}
